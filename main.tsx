90b42c505ca861f1379fb27bb1e5e4772424b1b9
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2022-01-09 14:38:20 +0900

    [update] apiつくった

--- main.tsx
+++ main.tsx
@@ -1,81 +1,81 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import * as postgres from "https://deno.land/x/postgres@v0.14.2/mod.ts";
 
-// Get the connection string from the environment variable "DATABASE_URL"
 const databaseUrl = Deno.env.get("DATABASE_URL")!;
 
-// Create a database pool with three connections that are lazily established
 const pool = new postgres.Pool(databaseUrl, 3, true);
-
-// Connect to the database
 const connection = await pool.connect();
 try {
-  // Create the table
   await connection.queryObject`
-    CREATE TABLE IF NOT EXISTS todos (
+    CREATE TABLE IF NOT EXISTS messages (
       id SERIAL PRIMARY KEY,
-      title TEXT NOT NULL
+      username TEXT NOT NULL,
+      message TEXT NOT NULL,
+      likes INTEGER NOT NULL DEFAULT 0,
+      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
     )
   `;
 } finally {
-  // Release the connection back into the pool
   connection.release();
 }
 
-serve(async (req) => {
-  // Parse the URL and check that the requested endpoint is /todos. If it is
-  // not, return a 404 response.
+serve(async (req: Request) => {
   const url = new URL(req.url);
-  if (url.pathname !== "/todos") {
+  if (!(url.pathname === "/messages" || (url.pathname === "/messages/like" && url.search.startsWith("id=")))) {
     return new Response("Not Found", { status: 404 });
   }
 
-  // Grab a connection from the database pool
   const connection = await pool.connect();
 
   try {
     switch (req.method) {
-      case "GET": { // This is a GET request. Return a list of all todos.
-        // Run the query
+      case "GET": {
+        // 全メッセージの取得
         const result = await connection.queryObject`
-          SELECT * FROM todos
+          SELECT * FROM messages
         `;
 
-        // Encode the result as JSON
         const body = JSON.stringify(result.rows, null, 2);
 
-        // Return the result as JSON
         return new Response(body, {
           headers: { "content-type": "application/json" },
         });
       }
-      case "POST": { // This is a POST request. Create a new todo.
-        // Parse the request body as JSON. If the request body fails to parse,
-        // is not a string, or is longer than 256 chars, return a 400 response.
-        const title = await req.json().catch(() => null);
-        if (typeof title !== "string" || title.length > 256) {
-          return new Response("Bad Request", { status: 400 });
+
+      case "POST": {
+        // 新しいメッセージの投稿
+        if (url.pathname === "/messages") {
+          const { username, message } = await req.json().catch(() => null);
+
+          await connection.queryObject`
+            INSERT INTO messages (username, message) VALUES (${username}, ${message})
+          `;
+
+          return new Response("", { status: 201 });
         }
 
-        // Insert the new todo into the database
-        await connection.queryObject`
-          INSERT INTO todos (title) VALUES (${title})
-        `;
+        // メッセージにlikeをつける
+        if (url.pathname === "/messages/like") {
+          const params = new URLSearchParams(url.search)
+          const id = params.get("id")
 
-        // Return a 201 Created response
-        return new Response("", { status: 201 });
+          await connection.queryObject`
+            UPDATE messages SET likes=likes+1 WHERE id=${id}
+          `
+
+          return new Response("", { status: 201 });
+        }
       }
-      default: // If this is neither a POST, or a GET return a 405 response.
+      default:
         return new Response("Method Not Allowed", { status: 405 });
     }
+
   } catch (err) {
     console.error(err);
-    // If an error occurs, return a 500 response
     return new Response(`Internal Server Error\n\n${err.message}`, {
       status: 500,
     });
   } finally {
-    // Release the connection back into the pool
     connection.release();
   }
 });
