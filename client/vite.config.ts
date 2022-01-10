b03dd52b486822c4e12feb1c5b3fdf83f3198379
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2022-01-10 19:31:33 +0900

    [update] ビルド版をdocsディレクトリに移動

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir:"../dist"
+    outDir:"../docs"
   },
   plugins: [vue()]
 })
