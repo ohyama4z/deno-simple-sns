7898d73b9747918768d63a6217b2bea4296bc5fc
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2022-01-10 19:54:16 +0900

    [build] baseを設定

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir:"../docs"
+    outDir: "../docs",
+    assetsDir: "./assets",
+    base:"./"
   },
   plugins: [vue()]
 })
